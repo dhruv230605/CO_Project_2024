75410685158a0b2778e7281e28452c148fd5a425
dhruv230605 <162973752+dhruv230605@users.noreply.github.com>
2024-04-18 01:08:51 +0530

    Update Simulator.py

--- Simulator.py
+++ Simulator.py
@@ -1,3 +1,17 @@
+registers = {'zero' : '00000', 'ra': '00001', 'sp': '00010', 'gp': '00011', 'tp': '00100', 't0':'00101', 't1': '00110', 't2': '00111','s0': '01000', 'fp': '01000', 's1': '01001','a0': '01010', 'a1': '01011', 'a2': '01100', 'a3': '01101', 'a4': '01110', 'a5': '01111','a6':'10000',  'a7':'10001', 's2': '10010', 's3': '10011', 's4': '10100', 's5': '10101', 's6': '10110', 's7': '10111', 's8': '11000', 's9': '11001', 's10': '11010', 's11': '11011','t3': '11100', 't4': '11101', 't5':'11110', 't6': '11111'}
+value  = {}
+for i in registers:
+    value[registers[i]] = 0
+value['00010']=256
+memory={}
+PC={}
+funct3={"add":"000","sub":"000","sll":"001","slt":"010","sltu":"011","xor":"100",
+        "srl":"101","or":"110","and":"111","lw":"010","addi":"000","sltiu":"011",
+        "jalr":"000","sw":"010",'beq':"000",'bne':"001",'blt':"100",'bge':"101",
+        'bltu':"110",'bgeu':"111"}
+funct7={"add":"0000000","sub":"0100000","sll":"0000000","slt":"0000000","sltu":"0000000","xor":"0000000",
+        "srl":"0000000","or":"0000000","and":"0000000"}
+types={'1100011':'B',"1101111":'J',"0110111":'U',"0010111":'U',"0100011":'S',"0000011":'I',"1100111":'I',"0010011":'I' ,"0110011":'R'}
 class J_type:
     def __init__(self,ins:str,pc:int) -> None:
         super().__init__(ins,pc)
@@ -10,15 +24,11 @@ class J_type:
         #a = (a >> 4) << 4
         registers[self.rd]=self.pc+4
         print('rd = ', self.rd, ' value at rd = ', registers[self.rd])
-        #self.rd=self.pc+4
-        #imm = imm[::-1]
-        #imm = imm[20] + imm[10:0:-1] +imm[11] + imm[19:11:-1]
+
         print('unscrambled imm = ', self.imm)
         self.imm = self.imm[10:0:-1] + self.imm[11] + self.imm[19:11:-1] + self.imm[0]
         self.imm=self.imm[::-1] + '0'
-        # self.imm = self.imm[0] + self.imm[13::] + self.imm[12] + self.imm[1:12]
-        # self.imm=self.imm[20]+self.imm[10:0:-1]+self.imm[11]+self.imm[19:11:-1]
-        # self.imm[::-1]
+
         print('imm binary = ', self.imm)
         if self.imm[0] == '0':
             self.pc=self.pc+int(self.imm,2)
@@ -56,6 +66,81 @@ def typeU(instruction):
     u_inst = U_type(instruction)
     u_inst.execute()
 
+#type S
+def execute_typeS(instruction, value, pc, mem):
+    register1=instruction[12:17]
+    valreg1=value[register1]
+    reg2=instruction[7:12]
+    valreg2=value[reg2]
+    immval=instruction[0:7]+instruction[20:25]
+    mem[converttohex(65536 + (valreg1+int(twos_complement(immval,12)))*4)]=valreg2
+    pc+=4
+    return pc
+
+#typeB
+def execute_typeB(instruction, pc, value):
+    immediate=instruction[1:7]+instruction[20:25]
+    immediate=immediate[0]+immediate[-1]+immediate[1:11]
+    immediate+="0"
+    register2=instruction[7:12]
+    register1=instruction[12:17]
+    if(instruction[17:20]=="000"):
+        if(value[register2]==value[register1]):
+            pc=pc+int(twos_complement(immediate,12))
+            return pc
+        else:
+            pc+=4
+            return pc
+    elif(instruction[17:20]=="001"):
+        if(value[register2]!=value[register1]):
+            pc=pc+int(twos_complement(immediate,12))
+            return pc
+        else:
+            pc+=4
+            return pc
+    elif(instruction[17:20]=="100"):
+        if(value[register2]>value[register1]):
+            pc=pc+int(twos_complement(immediate,12))
+            return pc
+        else:
+            pc+=4
+            return pc
+    elif(instruction[17:20]=="101"):
+        if(value[register2]<=value[register1]):
+            pc=pc+int(twos_complement(immediate,12))
+            return pc
+        else:
+            pc+=4
+            return pc
+    elif(instruction[17:20]=="110"):
+        unsignedreg1=2**32+value[register1] if value[register1] <0 else value[register1]
+        unsignedreg2=2**32+value[register2] if value[register2] <0 else value[register2]
+        if(unsignedreg1<unsignedreg2):
+            pc=pc+int(twos_complement(immediate,12))
+            return pc
+        else:
+            pc+=4
+            return pc
+    elif(instruction[17:20]=="111"):
+        unsignedreg1=2**32+value[register1] if value[register1] <0 else value[register1]
+        unsignedreg2=2**32+value[register2] if value[register2] <0 else value[register2]
+        if(unsignedreg1>=unsignedreg2):
+            pc=pc+int(twos_complement(immediate,12))
+            return pc
+        else:
+            pc+=4
+            return pc
+
+def twos_complement(binary_str, num_bits):
+    if binary_str[0] == '1':
+        temp = ''.join('1' if bit == '0' else '0' for bit in binary_str)
+        temp_decimal = int(temp, 2)
+        twos_comp = -(temp_decimal+ 1)
+        return twos_comp
+    else:
+        return int(binary_str, 2)
+
+#typeR
 def findTwoscomplement(str):
     n = len(str)
     i = n - 1
@@ -193,7 +278,7 @@ def typeR(x):
     elif funct3=="001":
         and_func(rd,rs1,rs2)
 
-
+#typeI
 class I_type():
     def _init_(self,ins:str,pc:int) -> None:
         super()._init_(ins,pc)
@@ -232,13 +317,7 @@ def typeI(instruction):
 
 
 # Other code remains unchanged
-registers = {'zero' : '00000', 'ra': '00001', 'sp': '00010', 'gp': '00011', 'tp': '00100', 't0':'00101', 't1': '00110', 't2': '00111','s0': '01000', 'fp': '01000', 's1': '01001','a0': '01010', 'a1': '01011', 'a2': '01100', 'a3': '01101', 'a4': '01110', 'a5': '01111','a6':'10000',  'a7':'10001', 's2': '10010', 's3': '10011', 's4': '10100', 's5': '10101', 's6': '10110', 's7': '10111', 's8': '11000', 's9': '11001', 's10': '11010', 's11': '11011','t3': '11100', 't4': '11101', 't5':'11110', 't6': '11111'}
-value  = {}
-for i in registers:
-    value[registers[i]] = 0
-memory={}
-PC={}
-types={'1100011':'B',"1101111":'J',"0110111":'U',"0010111":'U',"0100011":'S',"0000011":'I',"1100111":'I',"0110011":'R'}
+
 
 def gettype(a):
     return types[a[-7:]]
@@ -257,7 +336,8 @@ def convertbin(decimal_num, num_bits):
 
         return binary_num
 
-def printreg():
+def printreg(a):
+    print('0b'+converttobin(a,32),end=' ')
     for i in value:
         print('0b'+converttobin(value[i],32))
 
@@ -279,20 +359,23 @@ for i in instructions:
 pcount=4
 while(pcount<=4*len(instructions)):
     currentinstruction=PC[pcount]
+    if(currentinstruction=="00000000000000000000000001100011"):
+        break
     typ=gettype(currentinstruction)
     if typ=='R':
         typeR(currentinstruction)
+        pcount+=4
     elif typ=='B':
-        typeB(currentinstruction)
+        pcount=execute_typeB(currentinstruction,pcount, value)
     elif typ=='J':
         typeJ(currentinstruction)
     elif typ=='I':
         typeI(currentinstruction)
     elif typ=='S':
-        typeS(currentinstruction)
+        pcount=execute_typeS(currentinstruction, value, pcount, memory)
     elif typ=='U':
         typeU(currentinstruction)
-    printreg()
+    printreg(pcount-4)
 
-printreg()
+printreg(pcount-4)
 printmemory()
